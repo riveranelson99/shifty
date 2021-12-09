67f000968d42bef9d0fa1eb3ea612409ddb48192
Stephen Thompson <s31@umbc.edu>
2021-12-09 14:08:31 -0600

    pull

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -1,4 +1,4 @@
-const db = require('./config/connection');
+const db = require('./connection');
 const { User, Post, Job } = require('../models');
 
 db.once('open', async () => {
@@ -10,22 +10,22 @@ db.once('open', async () => {
     {
       title: 'Interesting tidbit',
       content: 'Did you know a margarita has different flavors',
-      datePosted: 12/13/21,
+      datePosted: '12/13/21',
     },
     {
       title: 'Did you know',
       content: 'Liquor is a liquid',
-      datePosted: 12/09/21,
+      datePosted: '12/09/21',
     },
     {
       title: 'Here\'s a great recipe to try',
       content: 'Add a bay leaf to blueberry vodka.  You\'re welcome!',
-      datePosted: 12/10/21,
+      datePosted: '12/10/21',
     },
     {
       title: 'Article',
       content: 'I read a great article on trying new flavor combinations.  If you have time, I recommend it.',
-      datePosted: 12/01/21,
+      datePosted: '12/01/21',
     },
     {
       title: 'Great experience',
@@ -35,7 +35,7 @@ db.once('open', async () => {
     {
       title: 'Steer clear',
       content: 'I picked up a shift at NorthEnd, and will never return.  The manager was very rude the entire shift, and didn\'t trust me to make a drink by myself.',
-      datePosted: 12/05/21,
+      datePosted: '12/05/21',
     },   
   ]);
 
@@ -45,48 +45,48 @@ db.once('open', async () => {
 
 
   //// JOB SEEDING
-  await Job.deletMany();
+  await Job.deleteMany();
 
   const jobs = await Job.insertMany([
     {
       jobName: 'Popup',
       description: 'Open bar at a family reunion.',
       rate: 35,
-      datePosted: 12 / 12 / 21,
-      startDate: 12 / 20 / 21,
-      endDate: 12 / 20 / 21,
+      datePosted: '12 / 12 / 21',
+      startDate: '12 / 20 / 21',
+      endDate: '12 / 20 / 21',
     },
     {
       jobName: 'Chilis',
       description: 'Bartending 4pm-12pm',
       rate: 15,
-      datePosted: 12 / 9 / 21,
-      startDate: 12 / 11 / 21,
-      endDate: 12 / 14 / 21,
+      datePosted: '12 / 9 / 21',
+      startDate: '12 / 11 / 21',
+      endDate: '12 / 14 / 21',
     },
     {
       jobName: 'Portillos',
       description: 'Christmas party with cash bar',
       rate: 60,
-      datePosted: 12 / 07 / 21,
-      startDate: 12 / 23 / 21,
-      endDate: 12 / 24 / 21,
+      datePosted: '12 / 07 / 21',
+      startDate: '12 / 23 / 21',
+      endDate: '12 / 24 / 21',
     },
     { 
       jobName: 'Eberly',
       description: 'Christmas party with cash bar',
       rate: 60,
-      datePosted: 12 / 09 / 21,
-      startDate: 12 / 23 / 21,
-      endDate: 12 / 24 / 21,
+      datePosted: '12 / 09 / 21',
+      startDate: '12 / 23 / 21',
+      endDate: '12 / 24 / 21',
     },
     {
       jobName: 'Cocktails and Culture',
       description: 'Popup at Museum of Science and Industry',
       rate: 55,
-      datePosted: 12 / 01 / 21,
-      startDate: 12 / 11 / 21,
-      endDate: 12 / 11 /21
+      datePosted: '12 / 01 / 21',
+      startDate: '12 / 11 / 21',
+      endDate: '12 / 11 /21'
     }
   ]);
 
@@ -144,7 +144,7 @@ db.once('open', async () => {
       employer: false,
       posts: [
         {
-          posts: [posts[6]._id]
+          posts: [posts[5]._id]
         }
       ],
     },
