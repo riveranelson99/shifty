7067073f20c1d2512b3cb62c1273eb3a0601f10e
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-12-10 11:55:45 -0600

    updating local with current repo

--- client/src/pages/Jobs.js
+++ client/src/pages/Jobs.js
@@ -2,4 +2,28 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import 
+import JobList from '../components/JobList';
+import { QUERYNAMINGCONVENTION } from '../utils/queries';
+
+const Job = () => {
+  const { jobId } = useParams();
+
+  const { loading, data } = useQuery(QUERYNAMINGCONVENTION, {
+    variables: { jobId: jobId },
+  });
+
+  const job = data?.job || {};
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h2>
+        Available Jobs!
+      </h2>
+
+      {job.name?.length > 0 && <JobList name={job.name} />}
+    </div>
+  )
+}
