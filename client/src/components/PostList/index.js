fe224aeef6a8cf01a2f70a21f2b8eee3dd5f90d4
Stephen Thompson <s31@umbc.edu>
2021-12-10 12:17:00 -0600

    pull

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const JobList = ({ jobs }) => {
-    if (!jobs.length) {
-        return <h3>No Jobs Yet</h3>
+const PostList = ({ posts }) => {
+    if (!posts.length) {
+        return <h3>No Posts Yet</h3>
     }
 
     return (
         <div>
           <h3 className="text-primary">{title}</h3>
           <div className="flex-row justify-space-between my-4">
-            {jobs &&
-              jobs.map((job) => (
-                <div key={job._id} className="col-12 col-xl-6">
+            {posts &&
+              posts.map((post) => (
+                <div key={post._id} className="col-12 col-xl-6">
                   <div className="card mb-3">
                     <h4 className="card-header bg-dark text-light p-2 m-0">
-                      {job.jobName} <br />
+                      {post.title} <br />
                       <span className="text-white" style={{ fontSize: '1rem' }}>
-                        Description:
-                        {job.description}
+                        Content:
+                        {post.content}
                       </span>
                     </h4>
                   </div>
