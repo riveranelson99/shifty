465788b435fac6e237dad3acdbdbcb39d51bdc98
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-12-10 10:33:34 -0600

    login and register work

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,57 +1,54 @@
 const { gql } = require('apollo-server-express');
 // brings in graph ql 
 const typeDefs = gql`
-type User { 
-    _id: ID!
-    username: String!
-    email: String 
-    workplaces: String
-    rate: Int! 
-}
+    type User { 
+        _id: ID!
+        username: String!
+        email: String 
+        workplaces: String
+        rate: Int! 
+    }
 
+    type Post {
+        _id: ID!
+        title: String!
+        content: String! 
+        date: String!
+    }
 
-type Post {
-    _id: ID!
-    title: String!
-    content: String! 
-    date: String!
-}
+    type Job {
+        _id: ID!
+        jobTitle: String!
+        description: String! 
+        rate: Int!
+        startDate: String!
+        endDate: String!
+    }
 
-type Job {
-    _id: ID!
-    jobTitle: String!
-    description: String! 
-    rate: Int!
-    startDate: String!
-    endDate: String!
-}
+    type Auth {
+        token: ID! 
+        user: User
+    }
 
-type Auth {
-    token: ID! 
-    user: User
-}
+    type Query {
+        me: User
+        user(username: String!): User
+        posts: [Post]!
+        post(postId: ID!): Post
+        jobs: [Job]!
+        job(jobId: ID!): Job
+    }
 
-
-type Query {
-    me: User
-    user(username: String!): User
-    posts: [Post]!
-    post(postId: ID!): Post
-    jobs: [Job]!
-    job(jobId: ID!): Job
-}
-
-type Mutation {
-    login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    addPost(postId: ID!, postTitle: String!, postContent: String!): User
-    addJob(jobId: ID!, jobTitle: String!, jobDescription: String!, jobRate: Int!, jobStartDate: String!, jobEndDate: String!): User
-    # editPost()
-    # editJob()
-    deletePost(postId: ID!): Post
-    deleteJob(jobId: ID!): Job
-}
-
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addPost(postId: ID!, postTitle: String!, postContent: String!): User
+        addJob(jobId: ID!, jobTitle: String!, jobDescription: String!, jobRate: Int!, jobStartDate: String!, jobEndDate: String!): User
+        # editPost()
+        # editJob()
+        deletePost(postId: ID!): Post
+        deleteJob(jobId: ID!): Job
+    }
 `;
 
 module.exports = typeDefs
