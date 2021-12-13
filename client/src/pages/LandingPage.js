6ef83da95137adee760f2dbfcae46b617475fa46
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-12-13 12:54:12 -0600

    fixed styling on footer, header name, and landing page

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,14 +1,27 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
+import { Box, Typography } from '@mui/material';
 
 export default function LandingPage() {
   return (
     // static header holding nav bar
-    <div>
-      <h2>Shifty is a job marketplace used to fill in last minute bartending positions for events teams, pop ups or restaurants.</h2>
-      <p>Are you a bartender looking for a job?  You've come to the right place.</p>
-      <p>Are you an employer looking for a bartender?  Take a look at the talent and take your pick</p>
-    </div>
+    <Box sx={{ width: "85vw", margin: 4 }}>
+      <Typography id="landingPage-heading" variant="h4" component="div" align="center" marginBottom="4" >
+        Shifty is a job marketplace used to fill in last minute bartending positions for events for teams, pop ups or restaurants.
+      </Typography>
+      <Typography id="landingPage-body" variant="body1" component="div" align="center" lineHeight="2" >
+      <br />
+      Are you a bartender looking for a job?  You've come to the right place.
+      <br />
+      Are you an employer looking for a bartender?  Take a look at the talent and take your pick.
+      </Typography>
+
+    </Box>
+    // <div>
+    //   <h2>Shifty is a job marketplace used to fill in last minute bartending positions for events teams, pop ups or restaurants.</h2>
+    //   <p>Are you a bartender looking for a job?  You've come to the right place.</p>
+    //   <p>Are you an employer looking for a bartender?  Take a look at the talent and take your pick</p>
+    // </div>
+    // </Box>
     // static footer
   );
 }
